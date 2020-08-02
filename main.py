be0d173caf78fc394d4858f393a74ee4707abca8
fepz <franpaez+github@gmail.com>
2020-08-02 20:25:12 -0300

    Add arguments.

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from argparse import ArgumentParser
 from tkinter import Tk
 from gui.maingui import MainGui
+from cli.MultipleSimulation import MultipleSimulationCli
 from cli.SingleSimulation import SingleSimulationCli
-
 
 def get_args():
     """ Command line arguments """
     parser = ArgumentParser()
-    parser.add_argument("--gui", help="Use GUI.")
+    parser.add_argument("--gui", action='store_true', help="Use GUI.")
     parser.add_argument("--file", type=str, help="File with RTS.")
-    parser.add_argument("--rts", type=int, help="RTS number inside file.")
+    parser.add_argument("--rts", type=str, help="RTS number inside file.")
     parser.add_argument("--instance-count", type=int, help="Number of task instances to simulate.")
-    parser.add_argument("--ss-method", nargs='+', type=str, help="Slack Stealing method.")
+    parser.add_argument("--ss-methods", nargs='+', type=str, help="Slack Stealing methods.")
     return parser.parse_args()
 
 
@@ -22,7 +22,8 @@ def main():
         MainGui(root)
         root.mainloop()
     else:
-        cli = SingleSimulationCli(args)
+        #cli = SingleSimulationCli(args)
+        cli = MultipleSimulationCli(args)
         cli.run_simulation()
 
 
