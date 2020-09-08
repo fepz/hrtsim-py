495283d275d21646665ba5a49e5ff14a28a1bc53
fepz <franpaez+github@gmail.com>
2020-09-08 10:37:41 -0300

    Add comment to mixrange function.

--- cli/cli.py
+++ cli/cli.py
@@ -6,6 +6,11 @@ from tabulate import tabulate
 
 
 def mixrange(s):
+    """
+    Create a list of numbers from a string. Ie: "1-3,6,8-10" into [1,2,3,6,8,9,10]
+    :param s: a string
+    :return: a list of numbers
+    """
     r = []
     for i in s.split(','):
         if '-' not in i:
