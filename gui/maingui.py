7c15737627f5c356cd7caee058b6e599343431a2
Francisco E. Paez <franpaez+github@gmail.com>
2020-09-18 09:52:33 -0300

    Import only needed functions.

--- gui/maingui.py
+++ gui/maingui.py
@@ -1,5 +1,4 @@
-from tkinter import *
-
+from tkinter import Frame, Label, Button
 from gui.SingleSimulation import SingleSimulationGui
 from gui.MultipleSimulationGui import MultipleSimulationGui
 
@@ -9,7 +8,7 @@ class MainGui:
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
-        master.title("HrtSim")
+        master.title("HrtSimPy")
 
         self.titleLabel = Label(frame, text="HrtSim v2").pack()
         self.singleSimButton = Button(frame, text="Single Simulation", command=SingleSimulationGui).pack()
