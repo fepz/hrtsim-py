805a483c7887b4797189a93d4b6f96b6f2f6daaa
Francisco E. Paez <franpaez+github@gmail.com>
2020-09-21 21:06:20 -0300

    Move mixrange to utils.py

--- cli/cli.py
+++ cli/cli.py
@@ -3,22 +3,7 @@ from concurrent.futures import ProcessPoolExecutor
 from tqdm.auto import tqdm
 from resources.xml import get_from_file
 from tabulate import tabulate
-
-
-def mixrange(s):
-    """
-    Create a list of numbers from a string. Ie: "1-3,6,8-10" into [1,2,3,6,8,9,10]
-    :param s: a string
-    :return: a list of numbers
-    """
-    r = []
-    for i in s.split(','):
-        if '-' not in i:
-            r.append(int(i))
-        else:
-            l, h = map(int, i.split('-'))
-            r += range(l, h+1)
-    return r
+from utils import mixrange
 
 
 def run_simulation(args):
