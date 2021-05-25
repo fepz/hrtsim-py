69b9a8f6a880f19ae65c58be18ffab59602f019a
fepz <franpaez+github@gmail.com>
2021-05-25 17:48:55 -0300

    Return exit code to the enviroment.

--- simulation.py
+++ simulation.py
@@ -1,7 +1,7 @@
 #!python
 
 from simulations.simslack import run_sim
-from argparse import ArgumentParser
+from argparse import ArgumentParser, FileType
 from utils import calculate_k, get_rts
 from schedtests import josephp
 import math
@@ -31,13 +31,14 @@ def run_single_simulation(rts, args):
     sim_result = run_sim(rts, params, callback=None, sink=True, retrieve_model=True)
 
     if sim_result["error"]:
-        print("Simulation failed!")
-        print("\t{0}".format(sim_result["error_msg"]))
+        print("Simulation failed: {0}".format(sim_result["error_msg"]))
+        sys.exit(1)
 
 
 def get_args():
     """ Command line arguments """
     parser = ArgumentParser()
+    parser.add_argument("--file", type=FileType('r'), default=sys.stdin, help="File with RTS.")
     parser.add_argument("--scheduler", nargs=1, type=str, help="Scheduling algorithm")
     parser.add_argument("--instance-count", type=int, help="Number of task instances to simulate.")
     parser.add_argument("--ss-methods", nargs='+', type=str, help="Slack Stealing methods.")
@@ -47,11 +48,11 @@ def get_args():
 def main():
     if not len(sys.argv) > 1:
         print("Error: no arguments.", file=sys.stderr)
-        sys.exit()
+        sys.exit(1)
 
     args = get_args()
 
-    for rts in get_rts(sys.stdin):
+    for rts in get_rts(args.file):
         run_single_simulation(rts, args)
 
 
