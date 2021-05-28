94296d475acf739b405f3a573714b1eb2fc29f07
Francisco E. Paez <franpaez+github@gmail.com>
2021-05-28 10:24:51 -0300

    Slack exception message prints all slack results.
    The slack exception message now show information about all the slack
    methods.

--- slack/SlackExceptions.py
+++ slack/SlackExceptions.py
@@ -6,4 +6,5 @@ class NegativeSlackException(Exception):
 
 class DifferentSlackException(Exception):
     def __init__(self, t, job, method, results):
-        Exception.__init__(self, 'Slack result differ for job {:s} at t={:f} for method {:s}.'.format(job.name, t, method))
+        err_str = "\n".join(["\t{0:} {1:} {2:} {3:}".format(m, r["slack"], r["ttma"], r["cc"]) for m, r in results])
+        Exception.__init__(self, 'Slack result differ for job {:s} at t={:f} for method {:s}:\n{:s}'.format(job.name, t, method, err_str))
