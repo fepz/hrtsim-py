25be2fc17be046656c5b1ce3ae7785693984480e
Francisco <franpaez+github@gmail.com>
2022-03-29 20:41:35 -0300

    Change format of replacement field to float.
    The format of the replacement field that indicates the simulation time at which the missed deadline happened is changed to float.

--- schedulers/MissedDeadlineException.py
+++ schedulers/MissedDeadlineException.py
@@ -1,3 +1,3 @@
 class MissedDeadlineException(Exception):
     def __init__(self, t, job):
-        Exception.__init__(self, 'Missed deadline for job {:s} at t={:d}'.format(job.name, t))
+        Exception.__init__(self, 'Missed deadline for job {:s} at t={:f}'.format(job.name, t))
